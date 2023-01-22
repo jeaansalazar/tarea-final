db8b42fc31e18101ae86ae3c216dbec3c44daa85
Alex Christian <alexchristianqr@gmail.com>
2023-01-22 16:29:33 -0500

    recuperacion de login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,18 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
-import {Observable, Observer} from "rxjs";
 import {Router} from "@angular/router";
 
-
-export interface FooGet {
-  // Define the form of the object in JSON format that your
-  // expect from the backend on get
+interface User {
+  first_name: string
+  last_name: string
+  password: string
+  phone: string
+  email: string
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
 
   constructor(private http: HttpClient, private router: Router) {
   }
@@ -37,9 +39,11 @@ export class AuthService {
     console.log('method: [registerUser]')
 
     return this.http.post('https://expensable-api.herokuapp.com/signup', user)
-      .subscribe((next) => {
+      .subscribe((next:any) => {
         console.log('next', next)
-        this.router.navigate(['/login'])
+        // user = next
+        alert('El usuario se ha creado correctamente')
+        this.router.navigate(['/login'], {queryParams:{email: next['email']}})
       }, error => {
         console.log('error', error)
         this.router.parseUrl('/register')
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../auth.service";
+import {ActivatedRoute, Route} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   email: string = 'test1@mail.com';
-  password: string = '123456';
+  password: string = '';
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.route.queryParams
+      .subscribe(params => {
+          console.log(params); // { order: "popular" }
+          this.email = params['email']
+        }
+      );
   }
 
   async login() {
-    console.log(this.email);
-    console.log(this.password);
+
     const response = await this.authService.doLogin(this.email, this.password)
     console.log({response})
   }
